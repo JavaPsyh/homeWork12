3e3c8a5c70c24dadb1ae33a507cfd2317a770bb1
JavaPsyh <psyhjava@gmail.com>
2019-06-19 23:36:09 +0300

    commit for verification

--- src/tasks/Integrate.java
+++ src/tasks/Integrate.java
@@ -1,4 +1,15 @@
 package tasks;
 
+import java.util.function.DoubleUnaryOperator;
+
 public class Integrate {
+    public static double integrate(DoubleUnaryOperator f, double a, double b) {
+        double tenPowSeven = Math.pow(10, 7);
+        double step = (b - a) / tenPowSeven;
+        double result = f.applyAsDouble(a) * step;
+        for (int i = 0; i < tenPowSeven; i++) {
+            result += f.applyAsDouble(a += step) * step;
+        }
+        return result;
+    }
 }
--- src/tasks/TernaryOperator.java
+++ src/tasks/TernaryOperator.java
@@ -1,4 +1,13 @@
 package tasks;
 
-public class TernaryOperator {
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class TernaryOperator<T, U> {
+    public static <T, U> Function<T, U> ternaryOperator(
+            Predicate<? super T> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse) {
+        return (z) -> condition.test(z) ? ifTrue.apply(z) : ifFalse.apply(z);
+    }
 }
